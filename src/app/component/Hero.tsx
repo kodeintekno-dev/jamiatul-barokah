dd59e14b5aa4c7e472d9f8cc73be94464f1e2f07
2S0r4 <nidsora25@gmail.com>
2025-08-06 14:00:40 +0700

    update hero mobile

--- src/app/component/Hero.tsx
+++ src/app/component/Hero.tsx
@@ -8,15 +8,17 @@ export default function Hero() {
       className="w-screen h-screen bg-cover font-[inter] flex items-center"
     >
       <section className="text-white p-10 font-bold flex flex-col gap-5">
-        <h2 className="text-[2em]">YAYASAN PENDIDIKAN NURUL QOLBU</h2>
-        <h1 className="text-[6.7em] leading-[90px]">
+        <h2 className="text-xl md:text-[2em]">
+          YAYASAN PENDIDIKAN NURUL QOLBU
+        </h2>
+        <h1 className="text-5xl md:text-[6.7em] md:leading-[90px]">
           PKBM JAMIATUL <br /> BAROKAH
         </h1>
         <div className="text-[2em]">
           <h3>Nomor Ijin Oprasional : 420.0005 –</h3>
           <h3>IPSPN Tahun 2022</h3>
         </div>
-        <h4 className="text-[1.6em] max-w-[800px]">
+        <h4 className="text-xl md:text-[1.6em] max-w-[800px]">
           Sukasari III RT. 03/01 No. 26B Kel. Sukasari, Kec. Bogor Timur 16142,
           Jawa Barat
         </h4>
