3782ab9ebf3d0db832101d0ec5ac8a28740126cc
2S0r4 <nidsora25@gmail.com>
2025-08-06 13:54:43 +0700

    update hero section

---
+++ src/app/component/Hero.tsx
@@ -0,0 +1,29 @@
+export default function Hero() {
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('/img/bg1.png')",
+      }}
+      className="w-screen h-screen bg-cover font-[inter] flex items-center"
+    >
+      <section className="text-white p-10 font-bold flex flex-col gap-5">
+        <h2 className="text-[2em]">YAYASAN PENDIDIKAN NURUL QOLBU</h2>
+        <h1 className="text-[6.7em] leading-[90px]">
+          PKBM JAMIATUL <br /> BAROKAH
+        </h1>
+        <div className="text-[2em]">
+          <h3>Nomor Ijin Oprasional : 420.0005 –</h3>
+          <h3>IPSPN Tahun 2022</h3>
+        </div>
+        <h4 className="text-[1.6em] max-w-[800px]">
+          Sukasari III RT. 03/01 No. 26B Kel. Sukasari, Kec. Bogor Timur 16142,
+          Jawa Barat
+        </h4>
+        <button className="bg-[#6CB531] rounded-full py-3 px-10 w-fit">
+          Daftar Sekarang
+        </button>
+      </section>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "./component/Hero";
+
 export default function Home() {
-  return;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
