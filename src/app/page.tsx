c504e5db48820d61c33f1fcaddfb890d3cc90007
DiqySH <margolang55@gmail.com>
2025-08-07 14:42:52 +0700

    nambahin about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "./component/Hero";
+import About from "./component/About";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
     </>
   );
 }
