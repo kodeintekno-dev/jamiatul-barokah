8670388abbd4048e2fb60736e7f9bc93f14ff4aa
2S0r4 <nidsora25@gmail.com>
2025-08-07 14:50:36 +0700

    add program

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "./component/Hero";
+import Program from "./component/Program";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Program />
     </>
   );
 }
