54b7acc28d13a7e8a9a6300dcb86795a316efbd3
DiqySH <margolang55@gmail.com>
2025-08-20 14:43:49 +0700

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Navbar from "@/components/Navbar";
 import Hero from "./component/Hero";
-import Program from "./component/Program";
+import Program from "./component/Footer";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Program />
     </>
   );
 }
