bbd57de815cac8323b4d06c0cf02dbb1c614f5ec
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-08-23 20:55:06 +0700

    fix: place footer in page.tsx instead of layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "./component/Hero";
 import About from "./component/About";
 import Program from "./component/Program";
-import Program from "./component/Footer";
+import Footer from "./component/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +11,7 @@ export default function Home() {
       <Hero />
       <About />
       <Program />
+      <Footer />
     </>
   );
 }
